bc708c21e9bfbffc260759cef6547d944508ed61
Luc Fueston <superluc@gmail.com>
2015-09-25 00:00:06 -0500

    Update mix.exs for hex

--- mix.exs
+++ mix.exs
@@ -4,17 +4,35 @@ defmodule Mex.Mixfile do
   def project do
     [app: :mex,
      version: "0.0.1",
-     elixir: "~> 1.1-beta",
+     elixir: "> 1.0.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package,
+     description: description
+    ]
+  end
+
+  def package do
+    [ # These are the default files included in the package
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Luc Fueston"],
+      contributors: ["Luc Fueston"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mrluc/mex",}]
+  end
+
+  def description do
+    """
+    Macro-expansion display helper for IEx.
+    """
   end
 
   # Configuration for the OTP application
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    []
   end
 
   # Dependencies can be Hex packages:
